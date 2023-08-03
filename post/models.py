f1b46d744bc34f9eb847c85fb2b27377c9fa5871
Shyam Chaudhary <chaudharyshyam410@gmail.com>
2023-08-03 17:31:32 +0545

    second code

--- post/models.py
+++ post/models.py
@@ -1,3 +1,49 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class PostStatus(models.TextChoices):
+    PUBLIC = ("public", "Public")
+    PRIVATE = ("private", "Private")
+
+class Post(models.Model):
+    picture = models.ImageField(upload_to="posts/")
+    caption = models.CharField(max_length=100, blank=True, null=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    status = models.CharField(
+        max_length=100,
+        choices=PostStatus.choices,
+        default=PostStatus.PRIVATE
+    )
+
+    
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        return str(self.id)
+    
+class Like(models.Model):
+    is_likes = models.BooleanField(default=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id)
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    
+class Comment(models.Model):
+    text = models.CharField(max_length=150)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+    
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True
+    )
--- useraccount/models.py
+++ useraccount/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    contact = models.CharField(max_length=255, blank=True, default="")
+    address = models.CharField(max_length=255, blank=True, default="")
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/",
+        blank=True,
+        null=True,
+    )
